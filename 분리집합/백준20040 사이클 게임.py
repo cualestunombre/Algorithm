17f48f9a635694e7f76194ca4e7481d6e10bb4e5
fearlessdeveloper <1dilumn0@gmail.com>
2023-09-23 16:21:39 +0900

    문제추가

---
+++ 분리집합/백준20040 사이클 게임.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(10000)
+n,m= map(int,input().split())
+l = [tuple(map(int,input().split())) for i in range(m)]
+parent = {}
+
+def findParent(i):
+   if parent[i] == i:
+       return i
+   else:
+       parent[i] = findParent(parent[i])
+       return parent[i]
+
+def union(u,v):
+    parentU,parentV = findParent(u),findParent(v)
+    parent[parentU] = parentV
+
+for i in range(n):
+    parent[i] = i
+
+turn = 1
+flag = False
+for x,y in l:
+    if findParent(x) == findParent(y):
+        flag = True
+        break
+    else:
+        union(x,y)
+    turn+=1
+
+if flag:
+    print(turn)
+else:
+    print(0)
+
+
+
+
